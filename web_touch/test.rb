357e2f5049a5ebc65a6eb6a818fc02d485f2c4ec
池田英輝 <85597833+EICHAN0413@users.noreply.github.com>
2021-07-08 17:39:45 +0900

    Update test.rb

--- web_touch/test.rb
+++ web_touch/test.rb
@@ -7,7 +7,8 @@ server = WEBrick::HTTPServer.new({
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'information.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'test.html.erb')
+server.mount('/information', WEBrick::HTTPServlet::ERBHandler, 'information.html.erb')
 server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 server.start
